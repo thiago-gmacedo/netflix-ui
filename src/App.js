9e23d9c61d4abfc27f487a18464441174a07fe3a
Thiago Gomes <thiagog.macedom@gmail.com>
2022-04-05 03:31:08 -0300

    feat: add more movies row on app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
   return (
     <>
       <Movies title="Netflix originals" movies={data.originals} />
+      <Movies title="Trending" movies={data.trending} />
+      <Movies title="Now playing" movies={data.now_playing} />
+      <Movies title="Popular" movies={data.popular} />
+      <Movies title="Top rated" movies={data.top_rated} />
+      <Movies title="Upcoming" movies={data.upcoming} />
     </>
   );
 }
